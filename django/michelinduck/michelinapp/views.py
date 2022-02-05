22decbf48004f87418fad5848270e354bee67f51
jamzhong15 <jamzhong@gmail.com>
2022-02-05 18:59:38 +0000

    add functionality

--- django/michelinduck/michelinapp/views.py
+++ django/michelinduck/michelinapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'michelinapp/index.html')
+
+
+def add_inrediant(request):
+    return
+
+
+def delete_ingrediant(request):
+    return
+
+
+def get_recipe(request):
+    ingredient = request.GET.get('ingredient')
+
+    return render(request, 'michelinapp/recipe.html', {'recipe': ingredient})
