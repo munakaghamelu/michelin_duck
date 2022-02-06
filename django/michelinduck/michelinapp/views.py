d506524853de82d72c7010eee028cbbe563bbfab
jamzhong15 <jamzhong@gmail.com>
2022-02-06 05:02:07 +0000

    new

--- django/michelinduck/michelinapp/views.py
+++ django/michelinduck/michelinapp/views.py
@@ -21,7 +21,23 @@ def get_recipe(request):
     ingredients = ingredients.split(",")
 
     ingID = Ingredients.objects.filter(name__in=ingredients)
-    for id in ingID:
-        print(id.id)
+    ingredients = []
+    for ingredient in ingID:
+        print(ingredient.ingredient_id)
+        print(ingredient.name)
+        ingredients.append(ingredient.ingredient_id)
+
+    pair = Junction.objects.filter(ingredient_id__in=ingredients)
+    recipes = []
+    for recipe_id in pair:
+        recipes.append(recipe_id.recipe_id)
+    print(recipes)
+
+    recipes = Recipes.objects.filter(id__in=recipes)
+    print(recipes)
+
+    for r in recipes:
+        print(r.recipe_id)
+        print(r.name)
 
     return render(request, 'michelinapp/recipe.html', {'recipe': ingredients})
--- django/michelinduck/preprocessing.py
+++ django/michelinduck/preprocessing.py
@@ -3,49 +3,54 @@ import numpy as np
 import re
 
 df_raw = pd.read_csv('reduced_RAW_recipes.csv')
-cols = ['name','minutes','n_steps','steps','description','ingredients','n_ingredients']
+cols = ['name', 'minutes', 'n_steps', 'steps',
+        'description', 'ingredients', 'n_ingredients']
 
 df_recipes = df_raw[cols]
 
 # Create Ingredients Table
 ingredients = df_recipes['ingredients']
 str_ingredients = ingredients.str.cat(sep=',')
-str_ingredients = str_ingredients.replace('[','')
-str_ingredients = str_ingredients.replace(']','')
-str_ingredients = str_ingredients.replace('\'','')
+str_ingredients = str_ingredients.replace('[', '')
+str_ingredients = str_ingredients.replace(']', '')
+str_ingredients = str_ingredients.replace('\'', '')
 list_ingredients = str_ingredients.split(',')
 
 unique_list = set()
 for x in list_ingredients:
     unique_list.add(x)
 
+unique_list = list(unique_list)
 df_ingredients = pd.DataFrame({'name': unique_list[0]}, index=[0])
-for i in range(1,len(unique_list)):
+for i in range(1, len(unique_list)):
     df_ingredients.loc[len(df_ingredients)] = unique_list[i]
 
-# prepare to add list of ingredient ids
-df_ingredients['id'] = str(np.arange(len(df_ingredients)))
+# # prepare to add list of ingredient ids
+# df_ingredients['id'] = np.arange(len(df_ingredients))
 
-# Create Recipes Table
-df_recipes['id'] = str(np.arange(len(df_recipes)))
+# # Create Recipes Table
+# df_recipes['id'] = np.arange(len(df_recipes))
 
-def get_recipes(x):
-    all_recipes = df_recipes.loc[df_recipes['ingredients'].str.contains(x)]
-    return all_recipes['id'].str.cat(sep=',')
-
-def get_ingredients(x):
-    all_ingredients = df_ingredients.loc[df_ingredients['recipe_ids'].str.contains(x)]
-    return all_ingredients['id'].str.cat(sep=',')
-
+df_ingredients['id'] = 0
+df_recipes['id'] = 0
 combinations = []
+print(df_ingredients.loc[0, 'name'])
 for i in range(len(df_ingredients)):
-    ingredient = df_ingredients.loc[i,'name']
+    ingredient = df_ingredients.loc[i, 'name']
+    df_ingredients.loc[i, 'id'] = i
     for j in range(len(df_recipes)):
-        recipe = str(df_recipes.loc[j,'ingredients'])
+        recipe = str(df_recipes.loc[j, 'ingredients'])
+        df_recipes.loc[j, 'id'] = j
         if ingredient in recipe:
-            combinations.append([i,j])
+            combinations.append([i, j])
+            # print(ingredient)
+            # print(recipe)
+            # print(i)
+            # print(j)
+            # break
+df_junction = pd.DataFrame(combinations, columns=[
+                           'ingredient_id', 'recipe_id'])
 
-df_junction = pd.DataFrame(combinations, columns=['ingredient_id','recipe_id'])
 
 df_recipes.to_csv('recipes.csv', index=False)
 df_ingredients.to_csv('ingredients.csv', index=False)
