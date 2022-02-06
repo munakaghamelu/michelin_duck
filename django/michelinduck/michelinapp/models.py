3415c783cfde954b21f77fa7c589a0fd5d11840c
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2022-02-06 05:31:55 +0000

    converted ids to char

--- django/michelinduck/michelinapp/models.py
+++ django/michelinduck/michelinapp/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import BLANK_CHOICE_DASH
@@ -5,6 +6,7 @@ from django.db.models.fields import BLANK_CHOICE_DASH
 # Create your models here.
 class Ingredients(models.Model):
     name=models.CharField(max_length=50)
+    ingredients_id=models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.name}'
@@ -17,13 +19,14 @@ class Recipes(models.Model):
     description=models.CharField(max_length=500)
     ingredients=models.CharField(max_length=1000)
     n_ingredients=models.IntegerField()
+    recipe_id=models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.name}'
 
 class Junction(models.Model):
-    ingredient_id=models.IntegerField()
-    recipe_id=models.IntegerField()
+    ingredient_id=models.CharField(max_length=20)
+    recipe_id=models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.ingredient_id} and {self.recipe_id}'
--- django/michelinduck/preprocessing.py
+++ django/michelinduck/preprocessing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import re
 
 df_raw = pd.read_csv('reduced_RAW_recipes.csv')
 cols = ['name', 'minutes', 'n_steps', 'steps',
@@ -25,29 +24,19 @@ df_ingredients = pd.DataFrame({'name': unique_list[0]}, index=[0])
 for i in range(1, len(unique_list)):
     df_ingredients.loc[len(df_ingredients)] = unique_list[i]
 
-# # prepare to add list of ingredient ids
-# df_ingredients['id'] = np.arange(len(df_ingredients))
 
-# # Create Recipes Table
-# df_recipes['id'] = np.arange(len(df_recipes))
-
-df_ingredients['id'] = 0
-df_recipes['id'] = 0
+df_ingredients['ingredient_id'] = ''
+df_recipes['recipe_id'] = ''
 combinations = []
 print(df_ingredients.loc[0, 'name'])
 for i in range(len(df_ingredients)):
     ingredient = df_ingredients.loc[i, 'name']
-    df_ingredients.loc[i, 'id'] = i
+    df_ingredients.loc[i, 'ingredient_id'] = str(i)
     for j in range(len(df_recipes)):
         recipe = str(df_recipes.loc[j, 'ingredients'])
-        df_recipes.loc[j, 'id'] = j
+        df_recipes.loc[j, 'recipe_id'] = str(j)
         if ingredient in recipe:
-            combinations.append([i, j])
-            # print(ingredient)
-            # print(recipe)
-            # print(i)
-            # print(j)
-            # break
+            combinations.append([str(i), str(j)])
 df_junction = pd.DataFrame(combinations, columns=[
                            'ingredient_id', 'recipe_id'])
 
